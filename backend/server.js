b1b2d0d2ddd199f33dd4f1cf8ba56e2b10ea3347
Fahim Rehman <rehmnjr@Fahims-MacBook-Air.local>
2025-08-04 16:39:52 +0530

    fix deploy2

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://askmydoc-backend-lr1q.onrender.com/', 'http://localhost:3000'],
+  origin: ['https://askmydoc-7xfu.onrender.com/', 'http://localhost:3000'],
   methods: ['GET', 'POST'],
   credentials: true
 }));
